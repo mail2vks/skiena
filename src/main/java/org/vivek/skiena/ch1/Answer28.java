1d33e046eb57a16352fef3985b28578742f2e1ef
Vivek <singh.kr.vivek@gmail.com>
2013-07-02 21:24:54 +0530

    Added caliper for benchmarking

---
+++ src/main/java/org/vivek/skiena/ch1/Answer28.java
@@ -0,0 +1,66 @@
+package org.vivek.skiena.ch1;
+
+import com.google.caliper.Benchmark;
+import com.google.caliper.runner.CaliperMain;
+
+import java.util.Scanner;
+
+public class Answer28 extends Benchmark{
+
+	private static long dividend = 1;
+	private static long divisor = 1;
+
+
+	private static void divideOne(long dividend, long divisor) {
+
+		int quotient = 0;
+
+		while (dividend > divisor) {
+			dividend -= divisor;
+			quotient++;
+		}
+		System.out.println("(divideOne): Answer: " + quotient);
+	}
+
+	private static void divideTwo(long dividend, long divisor) {
+
+		int quotient = 0;
+
+		while (dividend > divisor * 2) {
+			dividend -= 2 * divisor;
+			quotient += 2;
+		}
+
+		dividend -= divisor;
+		quotient++;
+
+		System.out.println("(divideTwo): Answer: " + quotient);
+	}
+
+	public void timeDivideOne(int repeats)
+	{
+		for(int i = 0; i < repeats; i++)
+		{
+			divideOne(dividend,divisor);
+		}
+	}
+
+	public void timeDivideTwo(int reps)
+	{
+		for(int i = 0; i < reps; i++)
+		{
+			divideTwo(dividend, divisor);
+		}
+	}
+
+	public static void main(String args[]){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter dividend:");
+		dividend = 23344555;//scanner.nextLong();
+		System.out.println("Enter divisor:");
+		divisor = 17;//scanner.nextLong();
+
+		CaliperMain.main(Answer28.class, new String[]{"-i","micro"});
+	}
+}
